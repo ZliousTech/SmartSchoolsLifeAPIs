3e04ac15c29ff822c87f31a24152cee081fe70b3
ZliousTech <zlioustech2016@gmail.com>
2026-03-28 22:03:44 +0200

    BaseController: Upgrade logger to log the actual methodName and request data

--- SmartSchoolAPI.BaseService/BaseController.cs
+++ SmartSchoolAPI.BaseService/BaseController.cs
@@ -2,8 +2,9 @@
 using SmartSchoolAPI.Entities;
 using System;
 using System.Net;
-using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 
 namespace SmartSchoolAPI.BaseService
@@ -28,7 +29,11 @@ namespace SmartSchoolAPI.BaseService
             }
             catch (Exception ex)
             {
-                _loggerService.Error(ex.ToString(), MethodBase.GetCurrentMethod().Name);
+                var methodName = $"{ex.TargetSite?.Name}.{ex.TargetSite?.DeclaringType?.FullName}";
+                var requestInfo = await BuildRequestInfoAsync();
+
+                _loggerService.Error($"Exception: {ex.ToString()}\n{requestInfo}", methodName);
+
                 return Content(HttpStatusCode.InternalServerError, new BaseResponseDTO<T>
                 {
                     IsSuccess = false,
@@ -36,6 +41,34 @@ namespace SmartSchoolAPI.BaseService
                     Message = "Please re-try again, if the error persist please contact Administrator."
                 });
             }
+        }
+
+        private async Task<string> BuildRequestInfoAsync()
+        {
+            var loggerBulider = new StringBuilder();
+
+            loggerBulider.AppendLine($"URL       : {Request.RequestUri}");
+            loggerBulider.AppendLine($"Method    : {Request.Method}");
+            loggerBulider.AppendLine($"Query     : {Request.RequestUri.Query}");
+
+            // Parse query string params into key-value pairs
+            var queryParams = HttpUtility.ParseQueryString(Request.RequestUri.Query);
+            foreach (string key in queryParams)
+            {
+                loggerBulider.AppendLine($"  [{key}] = {queryParams[key]}");
+            }
+
+            // Log request body (POST/PUT params)
+            if (Request.Content != null)
+            {
+                var body = await Request.Content.ReadAsStringAsync();
+                if (!string.IsNullOrWhiteSpace(body))
+                {
+                    loggerBulider.AppendLine($"Body      : {body}");
+                }
+            }
+
+            return loggerBulider.ToString();
         }
     }
 }
